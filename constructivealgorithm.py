a9a4fecb14e53823513f4f23d38a084579426d0a
Gabriel Souto <soutog4@gmail.com>
2022-10-07 09:09:41 -0300

    problems with constructive algorithm with numpy

--- constructivealgorithm.py
+++ constructivealgorithm.py
@@ -10,38 +10,46 @@ problem_instance = Instance(filename)
 
 
 def greedyforfeits(X, W, P, b, F, D):
-    S = np.a
+    S = np.array([])
     bres = b
-    ratio = []
+    ratio = np.array([])
 
-    while X != S:
+    while np.array_equal(S, X) == False:
         print(S)
         Xiter = []
         for i in X:
             if W[i] <= bres and i not in S:
-                Xiter.append(i)
+                Xiter = np.append(Xiter, i)
+                # Xiter.append(i)
 
-        if not Xiter:
+        if Xiter.size == 0:
             return S
 
-        print(f"Xiter: {Xiter}")
-
-        for i in Xiter:
+        for i in np.nditer(Xiter):
+            i = int(i)
             p_i = P[i]
             for pair in F:
                 if pair[1] in S:
+                    # print(pair)
+                    # print(np.argwhere(F == pair))
+                    # print(F)
+                    # print(pair[1])
+                    # print(S)
                     p_i = p_i - D[F.index(pair)]
+                    # p_i = p_i - D[np.argwhere(F == pair)]
 
             ratio_i = p_i / W[i]
 
-            ratio.append(ratio_i)
+            ratio = np.append(ratio, ratio_i)
+            # ratio.append(ratio_i)
 
-        i_star = argmax(ratio)
+        i_star = np.argmax(ratio)
 
         if ratio[i_star] < 0:
             return S
 
-        S.append(Xiter[i_star])
+        S = np.append(S, Xiter[i_star])
+        # S.append(Xiter[i_star])
 
         bres = bres - W[i_star]
 
--- teste.py
+++ teste.py
@@ -10,11 +10,12 @@ problem_instance = Instance(filename)
 
 
 def greedyforfeits(X, W, P, b, F, D):
-    S = np.empty_like(X)
+    S = np.array([])
     bres = b
-    ratio = np.empty_like(X)
+    ratio = np.array([])
 
     while np.array_equal(S, X) == False:
+        print(S)
         Xiter = []
         for i in X:
             if W[i] <= bres and i not in S:
@@ -25,22 +26,29 @@ def greedyforfeits(X, W, P, b, F, D):
             return S
 
         for i in np.nditer(Xiter):
+            i = int(i)
             p_i = P[i]
             for pair in F:
                 if pair[1] in S:
+                    # print(pair)
+                    # print(np.argwhere(F == pair))
+                    # print(F)
+                    # print(pair[1])
+                    # print(S)
                     p_i = p_i - D[F.index(pair)]
+                    # p_i = p_i - D[np.argwhere(F == pair)]
 
             ratio_i = p_i / W[i]
 
             ratio = np.append(ratio, ratio_i)
             # ratio.append(ratio_i)
 
-        i_star = argmax(ratio)
+        i_star = np.argmax(ratio)
 
         if ratio[i_star] < 0:
             return S
 
-        S = np.append(S, Xiter[i_start])
+        S = np.append(S, Xiter[i_star])
         # S.append(Xiter[i_star])
 
         bres = bres - W[i_star]
